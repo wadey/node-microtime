c89a091e97382434443ad2af85f688b67d7b20f6
Wade Simmons <wade@wades.im>
2011-11-07 10:44:51 -0800

    support node 0.6.x

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-var binding = require('./build/default/microtime')
+var binding
+
+//
+// Try catch here around multiple build paths to support
+// `node@0.4.x` and `node@0.6.x`.
+// (from https://github.com/indexzero/daemon.node)
+//
+try { binding = require('./build/default/microtime') }
+catch (ex) { binding = require('./build/Release/microtime') }
 
 exports.now = binding.now
 exports.nowDouble = binding.nowDouble
