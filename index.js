a204998037504ed156fdd1abbd36d7719d007641
Wade Simmons <wade@wades.im>
2012-03-13 18:36:48 -0700

    use node-gyp to build

--- index.js
+++ index.js
@@ -1,12 +1,4 @@
-var binding
-
-//
-// Try catch here around multiple build paths to support
-// `node@0.4.x` and `node@0.6.x`.
-// (from https://github.com/indexzero/daemon.node)
-//
-try { binding = require('./build/default/microtime') }
-catch (ex) { binding = require('./build/Release/microtime') }
+var binding = require('bindings')('microtime.node')
 
 exports.now = binding.now
 exports.nowDouble = binding.nowDouble
